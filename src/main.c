acd0850e82f47e9c463e8cf4efd48e92e248c023
slembcke <slembcke@gmail.com>
2021-01-30 15:52:34 -0600

    Progress on tile locking

--- src/main.c
+++ src/main.c
@@ -4,6 +4,10 @@
 #include "pixler.h"
 #include "common.h"
 
+static void debug_message(const char* str){
+	px_buffer_blit(NT_ADDR(0, 0, 25), str, strlen(str));
+}
+
 #define BG_COLOR 0x17
 static const u8 PALETTE[] = {
 	BG_COLOR, 0x1D, 0x30, 0x10,
@@ -81,22 +85,54 @@ static const u8 SYMBOL_GRID_POSY[] = {
 	0,
 };
 
-static const u8 TILE_BOX[] = {
+static const u8 METATILES[] = {
+	// Regular
 	0x80, 0x81, 0x82, 0x83,
 	0x90, 0x91, 0x92, 0x93,
 	0xA0, 0xA1, 0xA2, 0xA3,
 	0xB0, 0xB1, 0xB2, 0xB3,
+
+	0x84, 0x85, 0x86, 0x87,
+	0x94, 0x91, 0x96, 0x97,
+	0xA4, 0xA5, 0xA6, 0xA7,
+	0xB4, 0xB5, 0xB6, 0xB7,
+	
+	0x88, 0x89, 0x8A, 0x8B,
+	0x98, 0x91, 0x9A, 0x9B,
+	0xA8, 0xA9, 0xAA, 0xAB,
+	0xB8, 0xB9, 0xBA, 0xBB,
+	
+	// Locked
+	0xC0, 0xC1, 0xC2, 0x83,
+	0xD0, 0x91, 0xD2, 0x93,
+	0xE0, 0xE1, 0xE2, 0xA3,
+	0xB0, 0xB1, 0xB2, 0xB3,
+	
+	0xC4, 0xC5, 0xC6, 0x87,
+	0xD4, 0x91, 0xD6, 0x97,
+	0xE4, 0xE5, 0xE6, 0xA7,
+	0xB4, 0xB5, 0xB6, 0xB7,
+	
+	0xC8, 0xC9, 0xCA, 0x8B,
+	0xD8, 0x91, 0xDA, 0x9B,
+	0xE8, 0xE9, 0xEA, 0xAB,
+	0xB8, 0xB9, 0xBA, 0xBB,
 };
 
-static const u8 TILE_CIRCLE[] = {
-	0xC0, 0xC1, 0xC2, 0xC3,
-	0xD0, 0xD1, 0xD2, 0xD3,
-	0xE0, 0xE1, 0xE2, 0xE3,
-	0xF0, 0xF1, 0xF2, 0xF3,
+static const u8* METATILE_CHRS[] = {
+	METATILES + 0x00, METATILES + 0x00, METATILES + 0x00, NULL, NULL, METATILES + 0x30, METATILES + 0x30, NULL,
+	METATILES + 0x10, METATILES + 0x10, METATILES + 0x10, NULL, METATILES + 0x40, METATILES + 0x40, METATILES + 0x40, NULL,
+	METATILES + 0x20, METATILES + 0x20, METATILES + 0x20, NULL, METATILES + 0x50, METATILES + 0x50, METATILES + 0x50, NULL,
 };
 
-static const u8* METATILE_CHRS[] = {TILE_BOX, TILE_BOX, TILE_BOX, TILE_CIRCLE, TILE_CIRCLE};
-static const u8 METATILE_ATTRS[] = {0x00, 0x55, 0xAA, 0x55, 0xAA};
+#define PLAYER1_ATTR 0x55
+#define PLAYER2_ATTR 0xAA
+
+static const u8 METATILE_ATTRS[] = {
+	0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL, 0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL,
+	0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL, 0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL,
+	0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL, 0x00, PLAYER1_ATTR, PLAYER2_ATTR, NULL,
+};
 
 static const u16 TILE_NT_OFFS[] = {
 	0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000, 0x000,
@@ -120,17 +156,17 @@ static const u8 TILE_AT_OFFS[] = {
 	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 };
 
-// static const u8 GRID_INDICES[] = {
-// 	0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
-// 	0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
-// 	0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E,
-// 	0x21, 0x22, 0x23, 0x24, 0x25, 0x26,
-// 	0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E,
-// 	0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
-// };
+static const u8 GRID_INDICES[] = {
+	0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
+	0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+	0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E,
+	0x21, 0x22, 0x23, 0x24, 0x25, 0x26,
+	0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E,
+	0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
+};
 
 // TODO Temp data
-static u8 SYMBOL_GRID[64] = {
+static u8 symbol_grid[64] = {
 	0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
 	0x0, 0x0, 0x1, 0x2, 0x3, 0x0, 0x0, 0x0,
 	0x0, 0x4, 0x5, 0x6, 0x7, 0x0, 0x0, 0x0,
@@ -141,26 +177,57 @@ static u8 SYMBOL_GRID[64] = {
 	0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
 };
 
-static u8 TILE_GRID[64];
+#define TILE_PLAYER1_OWN_BIT 0x1
+#define TILE_PLAYER2_OWN_BIT 0x2
+#define TILE_OWNER_BITS (TILE_PLAYER1_OWN_BIT | TILE_PLAYER2_OWN_BIT)
+#define TILE_LOCKED_BIT 0x4
+#define TILE_ANIM_BITS 0x18
+#define TILE_BORDER_BIT 0x20
+#define TILE_PERIMETER_BITS (TILE_BORDER_BIT | TILE_OWNER_BITS)
+static u8 tile_grid[64];
+
+#define P TILE_PERIMETER_BITS
+static const u8 TILE_GRID_INIT[64] = {
+	P, P, P, P, P, P, P, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, 0, 0, 0, 0, 0, 0, P,
+	P, P, P, P, P, P, P, P,
+};
 
 static u8 FOOBAR[512];
 
-static void blit_tile(u8 tile){
+#define MOUSE_SPEED 0x280
+static u16 mouse_x, mouse_y;
+static u8 grid_idx, grid_tile;
+static u8 player_own_bit = TILE_PLAYER1_OWN_BIT;
+
+static void blit_tile(){
 	static u8 metatile;
 	static u16 nt_offs;
 	
-	nt_offs = TILE_NT_OFFS[tile];
-	metatile = TILE_GRID[tile];
+	nt_offs = TILE_NT_OFFS[grid_idx];
+	metatile = tile_grid[grid_idx];
 	
 	px_buffer_blit((NT_ADDR(0, 0, 0) + 0x00) + nt_offs, METATILE_CHRS[metatile] + 0x0, 4);
 	px_buffer_blit((NT_ADDR(0, 0, 0) + 0x20) + nt_offs, METATILE_CHRS[metatile] + 0x4, 4);
 	px_buffer_blit((NT_ADDR(0, 0, 0) + 0x40) + nt_offs, METATILE_CHRS[metatile] + 0x8, 4);
 	px_buffer_blit((NT_ADDR(0, 0, 0) + 0x60) + nt_offs, METATILE_CHRS[metatile] + 0xC, 4);
-	px_buffer_blit(AT_ADDR(0) + TILE_AT_OFFS[tile], METATILE_ATTRS + metatile, 1);
+	px_buffer_blit(AT_ADDR(0) + TILE_AT_OFFS[grid_idx], METATILE_ATTRS + metatile, 1);
 }
 
-#define MOUSE_SPEED 0x280
-static u16 mouse_x, mouse_y;
+static void debug_blit_grid(){
+	for(ix = 1; ix <= 6; ix++){
+		for(iy = 1; iy <= 6; iy++){
+			grid_idx = ix + iy*8;
+			blit_tile();
+			px_coro_yield(0);
+		}
+	}
+}
 
 static u8 tile_at_mouse(void){
 	u8 x = (mouse_x + 0x0000) >> 13;
@@ -173,19 +240,106 @@ static u8 tile_at_mouse(void){
 	}
 }
 
+static void animate_up(){
+	symbol_grid[grid_idx] -= 0x10;
+	tile_grid[grid_idx] -= 0x8;
+	blit_tile();
+	px_coro_yield(0);
+	px_coro_yield(0);
+}
+
+static void animate_down(){
+	symbol_grid[grid_idx] += 0x10;
+	tile_grid[grid_idx] += 0x8;
+	blit_tile();
+	px_coro_yield(0);
+	px_coro_yield(0);
+}
+
+static u8 selected_tile_count;
+
+static void click_tile(){
+	if(tile_grid[grid_idx] & TILE_ANIM_BITS){
+		debug_message("UP ");
+		px_debug_hex(grid_idx);
+		animate_up();
+		animate_up();
+		selected_tile_count--;
+	} else {
+		debug_message("DOWN ");
+		px_debug_hex(grid_idx);
+		animate_down();
+		animate_down();
+		selected_tile_count++;
+	}
+}
+
+static bool check_match(void){
+	return true;
+}
+
+static void assign_tiles(void){
+	for(grid_idx = 0; grid_idx < sizeof(tile_grid); grid_idx++){
+		grid_tile = tile_grid[grid_idx];
+		if((grid_tile & TILE_ANIM_BITS) && !(grid_tile & TILE_LOCKED_BIT)){
+			tile_grid[grid_idx] = (grid_tile & ~TILE_OWNER_BITS) | player_own_bit;
+			blit_tile();
+			px_coro_yield(0);
+		}
+	}
+}
+
+static void lock_tiles(void){
+	for(grid_idx = 0; grid_idx < sizeof(tile_grid); grid_idx++){
+		tmp = tile_grid[grid_idx];
+		
+		// Skip if the tile is not owned.
+		if((tmp & (player_own_bit | TILE_PERIMETER_BITS)) == player_own_bit){
+			tmp &= (tile_grid - 1)[grid_idx]; // Left
+			tmp &= (tile_grid + 1)[grid_idx]; // Right
+			tmp &= (tile_grid - 8)[grid_idx]; // Up
+			tmp &= (tile_grid + 8)[grid_idx]; // Down
+			if(tmp & player_own_bit){
+				tile_grid[grid_idx] |= TILE_LOCKED_BIT;
+				blit_tile();
+				px_coro_yield(0);
+			}
+		}
+	}
+}
+
+static void raise_tiles(void){
+	for(grid_idx = 0; grid_idx < sizeof(tile_grid); grid_idx++){
+		if(tile_grid[grid_idx] & TILE_ANIM_BITS){
+			animate_up();
+			animate_up();
+		}
+	}
+	
+	selected_tile_count = 0;
+}
+
 static void gameloop_player(void){
 	while(true){
 		if(JOY_LEFT (pad1.value)) mouse_x -= MOUSE_SPEED;
 		if(JOY_RIGHT(pad1.value)) mouse_x += MOUSE_SPEED;
 		if(JOY_DOWN (pad1.value)) mouse_y += MOUSE_SPEED;
 		if(JOY_UP   (pad1.value)) mouse_y -= MOUSE_SPEED;
-		px_spr(mouse_x >> 8, mouse_y >> 8, 0x02, 0x04);
 		
-		if(JOY_BTN_B(pad1.press)){
-			tmp = tile_at_mouse();
-			if(tmp){
-				TILE_GRID[tmp] = 1;
-				blit_tile(tmp);
+		if(JOY_BTN_B(pad1.release)){
+			grid_idx = tile_at_mouse();
+			if(grid_idx) click_tile();
+			
+			if(selected_tile_count == 4){
+				if(check_match()){
+				// 	shuffle_tiles();
+					assign_tiles();
+					lock_tiles();
+					raise_tiles();
+					break;
+				} else {
+					raise_tiles();
+				}
 			}
 		}
 		
@@ -193,42 +347,55 @@ static void gameloop_player(void){
 	}
 }
 
-static u8 GAMELOOP_CORO[256];
+static u8 gameloop_coro[128];
 static uintptr_t gameloop_body(uintptr_t _){
 	mouse_x = 0x8000, mouse_y = 0x8000;
 	
 	while(true){
+		debug_message("BLUE ");
+		player_own_bit = TILE_PLAYER1_OWN_BIT;
+		gameloop_player();
+		
+		debug_message("GREEN ");
+		player_own_bit = TILE_PLAYER2_OWN_BIT;
 		gameloop_player();
 	}
 }
 
-static void splash_screen(void){
+static void game_screen(void){
+	memcpy(tile_grid, TILE_GRID_INIT, sizeof(tile_grid));
+	
 	px_ppu_sync_disable();{
 		for(ix = 1; ix <= 6; ix++){
 			for(iy = 1; iy <= 6; iy++){
-				blit_tile(ix + iy*8);
+				grid_idx = ix + iy*8;
+				blit_tile();
 				px_buffer_exec();
 			}
 		}
 	} px_ppu_sync_enable();
 	
+	px_debug_hex_addr = NT_ADDR(0, 0, 24);
 	PX.scroll_y = 480 - 16;
 	PX.scroll_x = -36;
 		
-	// music_play(0);
-	
 	fade_from_black(PALETTE, 4);
 	
-	px_coro_init(gameloop_body, GAMELOOP_CORO, sizeof(GAMELOOP_CORO));
+	px_coro_init(gameloop_body, gameloop_coro, sizeof(gameloop_coro));
 	
 	while(true){
 		read_gamepads();
-		if(px_coro_resume(GAMELOOP_CORO, 0)) break;
+		px_debug_hex(selected_tile_count);
+		if(px_coro_resume(gameloop_coro, 0)) break;
 		
+		px_spr(mouse_x >> 8, mouse_y >> 8, 0x02, 0x04);
+		
+		// This loop is SUPER expensive. Do I care?
 		for(ix = 1; ix <= 6; ix++){
 			for(iy = 1; iy <= 6; iy++){
-				idx = SYMBOL_GRID[ix + iy*8] & 0xF;
-				px_spr(SYMBOL_GRID_POSX[ix], SYMBOL_GRID_POSY[iy], SYMBOL_ATTR[idx], SYMBOL_CHR[idx]);
+				idx = ix + iy*8;
+				tmp = symbol_grid[idx];
+				px_spr(SYMBOL_GRID_POSX[ix] + (tmp >> 4), SYMBOL_GRID_POSY[iy] + (tmp >> 4), SYMBOL_ATTR[tmp & 0xF], SYMBOL_CHR[tmp & 0xF]);
 			}
 		}
 		
@@ -237,8 +404,6 @@ static void splash_screen(void){
 		px_spr_end();
 		px_wait_nmi();
 	}
-	
-	splash_screen();
 }
 
 void main(void){
@@ -258,5 +423,5 @@ void main(void){
 	sound_init(&SOUNDS);
 	music_init(&MUSIC);
 	
-	splash_screen();
+	game_screen();
 }
